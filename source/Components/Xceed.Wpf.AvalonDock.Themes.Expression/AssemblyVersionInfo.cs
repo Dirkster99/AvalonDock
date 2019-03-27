322080247324006a00bb77296ba6b387fe311d93
Ioannis.Madenis <johnmadenis@gmail.com>
2019-03-27 16:39:58 +0100

    Fixed old styles for anchorable buttons to be compatible with material design library

--- source/Components/Xceed.Wpf.AvalonDock.Themes.Expression/AssemblyVersionInfo.cs
+++ source/Components/Xceed.Wpf.AvalonDock.Themes.Expression/AssemblyVersionInfo.cs
@@ -21,10 +21,10 @@
 internal static class _XceedVersionInfo
 {
   [System.Diagnostics.CodeAnalysis.SuppressMessage( "Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields" )]
-  public const string BaseVersion = "2.0";
+  public const string BaseVersion = "3.5";
   [System.Diagnostics.CodeAnalysis.SuppressMessage( "Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields" )]
   public const string Version = BaseVersion + 
-  ".0.0";
+  ".2.0";
   [System.Diagnostics.CodeAnalysis.SuppressMessage( "Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields" )]
   public const string PublicKeyToken = "ba83ff368b7563c6";
 
--- source/Components/Xceed.Wpf.AvalonDock.Themes.VS2013/AssemblyVersionInfo.cs
+++ source/Components/Xceed.Wpf.AvalonDock.Themes.VS2013/AssemblyVersionInfo.cs
@@ -21,10 +21,10 @@
 internal static class _XceedVersionInfo
 {
   [System.Diagnostics.CodeAnalysis.SuppressMessage( "Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields" )]
-  public const string BaseVersion = "2.9";
+  public const string BaseVersion = "3.5";
   [System.Diagnostics.CodeAnalysis.SuppressMessage( "Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields" )]
   public const string Version = BaseVersion + 
-  ".0.1";
+  ".2.0";
   [System.Diagnostics.CodeAnalysis.SuppressMessage( "Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields" )]
   public const string PublicKeyToken = "ba83ff368b7563c6";
 
