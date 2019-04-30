37296a6a6008c3846c177a7468273ff83732f140
ruofeng.wang <ruofeng.wang@deltaww.com>
2019-04-30 16:50:33 +0800

    Add automation test project.

---
+++ source/AutomationTest/Xceed.Wpf.AvalonDock.Test/AnchorablePaneTest.cs
@@ -0,0 +1,18 @@
+﻿namespace Xceed.Wpf.AvalonDock.Test
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Xceed.Wpf.AvalonDock.Test.TestHelpers;
+    using Xceed.Wpf.AvalonDock.Test.views;
+
+    [TestClass]
+    public class AnchorablePaneTest: AutomationTestBase
+    {
+        [TestMethod]
+        public void TempTest()
+        {
+            TestHost.SwitchToAppThread();
+
+            var window = WindowHelpers.CreateInvisibleWindowAsync<AnchorablePaneTestWindow>();
+        }
+    }
+}
