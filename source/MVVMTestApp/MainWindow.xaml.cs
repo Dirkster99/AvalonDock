cae8182c67ba8af0d2a4e46d5f4407fa7b9746fe
Dirkster99 <Dirk.Bahle@gmx.de>
2019-10-08 19:18:31 +0200

    Multitargeting netcore3 and net4.0 with MVVMTestApp

--- source/MVVMTestApp/MainWindow.xaml.cs
+++ source/MVVMTestApp/MainWindow.xaml.cs
@@ -1,20 +1,4 @@
-﻿/************************************************************************
-
-   AvalonDock
-
-   Copyright (C) 2007-2013 Xceed Software Inc.
-
-   This program is provided to you under the terms of the New BSD
-   License (BSD) as published at http://avalondock.codeplex.com/license 
-
-   For more features, controls, and fast professional support,
-   pick up AvalonDock in Extended WPF Toolkit Plus at http://xceed.com/wpf_toolkit
-
-   Stay informed: follow @datagrid on Twitter or Like facebook.com/datagrids
-
-  **********************************************************************/
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
