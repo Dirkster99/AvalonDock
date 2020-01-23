bdc85b0fb040e4dd3cf485a13c65e7b11d4bb481
Alexei Stukov <stukov.alexei.va@gmail.com>
2020-01-23 09:12:08 +0200

    Code style

--- source/Components/AvalonDock/DockingManager.cs
+++ source/Components/AvalonDock/DockingManager.cs
@@ -78,20 +78,20 @@ namespace AvalonDock
 		#region Properties
 
 		// Make autohide delay a configurable property
-        public static readonly DependencyProperty AutoHideDelayProperty = DependencyProperty.Register("AutoHideDelay", typeof(int), typeof(DockingManager),
+		public static readonly DependencyProperty AutoHideDelayProperty = DependencyProperty.Register("AutoHideDelay", typeof(int), typeof(DockingManager),
             new FrameworkPropertyMetadata(1500));
 
-        public int AutoHideDelay 
-        {
-            get 
-            {
-                return (int)GetValue(AutoHideDelayProperty);
-            }
-            set 
-            {
+		public int AutoHideDelay 
+		{
+			get 
+			{
+				return (int)GetValue(AutoHideDelayProperty);
+			}
+			set 
+			{
 				SetValue(AutoHideDelayProperty, value);
-            }
-        }
+			}
+		}
 
 		#region Layout
 
