40f8530602440909c90556f2a2f0e0993ab559f5
Dirkster99 <Dirk.Bahle@gmx.de>
2020-03-13 23:15:32 +0100

    Added in Code documentation.

--- source/Components/AvalonDock/Controls/LayoutDocumentItem.cs
+++ source/Components/AvalonDock/Controls/LayoutDocumentItem.cs
@@ -25,11 +25,10 @@ namespace AvalonDock.Controls
 		#endregion fields
 
 		#region Constructors
-
+		/// <summary>Class constructor</summary>
 		internal LayoutDocumentItem()
 		{
 		}
-
 		#endregion Constructors
 
 		#region Properties
@@ -42,7 +41,7 @@ namespace AvalonDock.Controls
 
 		/// <summary>
 		/// Gets or sets the <see cref="Description"/> property.  This dependency property 
-		/// indicates the description to display for the document item.
+		/// indicates the description to display (in the <see cref="NavigatorWindow"/>) for the document item.
 		/// </summary>
 		public string Description
 		{
