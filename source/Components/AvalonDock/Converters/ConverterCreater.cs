218e2425b576bad533a112a6ada913ea5adcd6ac
Dirkster99 <Dirk.Bahle@gmx.de>
2020-07-03 20:46:32 +0200

    Add in code docu

--- source/Components/AvalonDock/Converters/ConverterCreater.cs
+++ source/Components/AvalonDock/Converters/ConverterCreater.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 
 namespace AvalonDock.Converters
 {
+    /// <summary>
+    /// Implements a static factory pattern for instances of WPF converters
+    /// and ensures that every single converter is instanciated only once.
+    /// </summary>
     internal class ConverterCreater
     {
         #region Private fields
@@ -13,6 +17,14 @@ namespace AvalonDock.Converters
 
         #region Public methods
 
+        /// <summary>
+        /// Gets an available instance of the indicated type <see cref="{T}"/> or a brand new instance
+        /// if the indicated type was not created, yet (to be returned on next query of instance for type <see cref="{T}"/>).
+        /// 
+        /// This method ensures that every instance per class <see cref="{T}"/> is instanciated only once.
+        /// </summary>
+        /// <typeparam name="T">Type of class to be returned form this factory.</typeparam>
+        /// <returns></returns>
         public static T Get<T>() where T : new()
         {
             if (!ConverterMap.ContainsKey(typeof(T)))
