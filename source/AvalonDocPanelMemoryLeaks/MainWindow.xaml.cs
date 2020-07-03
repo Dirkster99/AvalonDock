72996958cd5f1c6ef22f95b2c2a3d1777069186e
刘晓青 <569132102@qq.com>
2020-07-03 11:18:21 +0800

    Add a project about #171

---
+++ source/AvalonDocPanelMemoryLeaks/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using AvalonDock.Layout;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AvalonDocPanelMemoryLeaks
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            UserControl content = new UserControl();
+            content.DataContext = new HeavyLoad();
+            LayoutDocument docDocument = new LayoutDocument();
+            docDocument.Content = content;
+            docGrup.Children.Add(docDocument);
+            docDocument.Closed += DocClosed;
+        }
+
+        private void DocClosed(object sender, EventArgs e)
+        {
+            GC.Collect();
+        }
+    }
+    public class HeavyLoad:INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public byte[] Load { get; set; } = new byte[100000000];//100MB
+
+       
+    }
+}
