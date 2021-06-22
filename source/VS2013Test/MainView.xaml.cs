d16dd36cd55c36a090fb30ba37e82c2ba1df1597
Олег Трушков <oleg_trushkov@hotmail.com>
2021-06-22 17:17:49 +0300

    Created test project.

---
+++ source/VS2013Test/MainView.xaml.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VS2013Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+		private void MinimizeClick(object sender, RoutedEventArgs e)
+		{
+			WindowState = WindowState.Minimized;
+		}
+
+		private void MaximizeClick(object sender, RoutedEventArgs e)
+		{
+			WindowState = WindowState.Maximized;
+		}
+
+		private void RestoreDownClick(object sender, RoutedEventArgs e)
+		{
+			WindowState = WindowState.Normal;
+		}
+
+		private void CloseClick(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+	}
+}
