d275b350ad720a5d95fcfebd5a713e13ea59199f
nmagni <nmagni@nmagni.net>
2022-05-03 14:13:19 +0200

    #345 fix refresh when moving floating windows

--- source/Components/AvalonDock/Controls/Shell/WindowChromeWorker.cs
+++ source/Components/AvalonDock/Controls/Shell/WindowChromeWorker.cs
@@ -418,13 +418,6 @@ namespace Microsoft.Windows.Shell
 			// Since we always want the client size to equal the window size, we can unconditionally handle it
 			// without having to modify the parameters.
 			handled = true;
-			if (wParam != IntPtr.Zero)
-			{
-				var client = (RECT)Marshal.PtrToStructure(lParam, typeof(RECT));
-				client.Bottom++;
-				Marshal.StructureToPtr(client, lParam, false);
-				return IntPtr.Zero;
-			}
 			return new IntPtr((int)WVR.REDRAW);
 		}
 
