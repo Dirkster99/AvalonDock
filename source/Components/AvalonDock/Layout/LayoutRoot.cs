4f4177d5cf9fbbba7992a5a159c4b4fa28725fa4
Trivalik <3148279+trivalik@users.noreply.github.com>
2022-05-11 07:51:42 +0200

    fix build

--- source/Components/AvalonDock/Layout/LayoutRoot.cs
+++ source/Components/AvalonDock/Layout/LayoutRoot.cs
@@ -12,6 +12,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Windows.Controls;
 using System.Windows.Markup;
