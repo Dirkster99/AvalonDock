ef2eae0f6b3f535f4103e191ccfb0936ef5fcf0c
pkindruk <pkindruk@gmail.com>
2022-12-05 14:50:04 +0300

    Add thread safety to xml serializer cache.

--- source/Components/AvalonDock/XmlSerializersCache.cs
+++ source/Components/AvalonDock/XmlSerializersCache.cs
@@ -1,20 +1,29 @@
 using System;
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Xml.Serialization;
 
 namespace AvalonDock
 {
 	internal static class XmlSerializersCache
 	{
-		private static readonly Dictionary<Type, XmlSerializer> s_cache = new Dictionary<Type, XmlSerializer>();
+		private static readonly object s_lock = new object();
+		private static readonly ConcurrentDictionary<Type, XmlSerializer> s_cache = new ConcurrentDictionary<Type, XmlSerializer>();
 
 
 		public static XmlSerializer GetSerializer(Type targetType)
 		{
-			if (!s_cache.TryGetValue(targetType, out var serializer))
+			if (s_cache.TryGetValue(targetType, out var serializer))
+				return serializer;
+
+			lock (s_lock)
 			{
-				serializer = XmlSerializer.FromTypes(new[] { targetType })[0];
-				s_cache.Add(targetType, serializer);
+				// we can have multiple threads waiting on lock
+				// one of them could create what we were looking for
+				if (!s_cache.TryGetValue(targetType, out serializer))
+				{
+					serializer = XmlSerializer.FromTypes(new[] { targetType })[0];
+					_ = s_cache.TryAdd(targetType, serializer);
+				}
 			}
 
 			return serializer;
