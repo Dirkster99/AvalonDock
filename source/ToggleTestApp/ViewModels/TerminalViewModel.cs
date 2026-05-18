f31c4896d1f6714a04e37ecef3d362132870f454
ALEU1C014278 <stefan.kneidl@alpsalpine.com>
2026-05-18 18:29:17 +0200

    [Feature] Replace EasyWindowsTerminalControl with Simple.Wpf.Terminal for AvalonDock compatibility

--- source/ToggleTestApp/ViewModels/TerminalViewModel.cs
+++ source/ToggleTestApp/ViewModels/TerminalViewModel.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows;
 using AvalonDock.Core;
 using AvalonDock.Mvvm;
 
@@ -5,6 +10,11 @@ namespace ToggleTestApp.ViewModels;
 
 public partial class TerminalViewModel : ToolboxBase
 {
+    private Process? _process;
+    private bool _running;
+
+    public ObservableCollection<TerminalLine> OutputLines { get; } = new();
+
     public TerminalViewModel()
     {
         Id = "Terminal";
@@ -14,4 +24,61 @@ public partial class TerminalViewModel : ToolboxBase
         IsOpenByDefault = true;
         Icon = ToolboxIcons.Terminal;
     }
+
+    public void StartProcess()
+    {
+        if (_running) return;
+
+        var psi = new ProcessStartInfo
+        {
+            FileName = "powershell.exe",
+            Arguments = "-NoLogo -NoProfile",
+            UseShellExecute = false,
+            RedirectStandardInput = true,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            CreateNoWindow = true,
+            WorkingDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)
+        };
+
+        _process = new Process { StartInfo = psi };
+        _process.OutputDataReceived += OnOutputReceived;
+        _process.ErrorDataReceived += OnOutputReceived;
+        _process.Start();
+        _process.BeginOutputReadLine();
+        _process.BeginErrorReadLine();
+        _running = true;
+    }
+
+    public void StopProcess()
+    {
+        _running = false;
+        if (_process != null && !_process.HasExited)
+        {
+            try { _process.Kill(); } catch { }
+        }
+        _process?.Dispose();
+        _process = null;
+    }
+
+    public void ExecuteLine(string line)
+    {
+        if (_process == null || _process.HasExited) return;
+        _process.StandardInput.WriteLine(line);
+    }
+
+    private void OnOutputReceived(object sender, DataReceivedEventArgs e)
+    {
+        if (e.Data == null) return;
+        Application.Current?.Dispatcher.BeginInvoke(() =>
+        {
+            OutputLines.Add(new TerminalLine(e.Data));
+        });
+    }
+}
+
+public class TerminalLine
+{
+    public string Value { get; }
+    public TerminalLine(string value) => Value = value;
 }
--- source/ToggleTestApp/Views/TerminalView.xaml.cs
+++ source/ToggleTestApp/Views/TerminalView.xaml.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Windows;
 using System.Windows.Controls;
+using ToggleTestApp.ViewModels;
 
 namespace ToggleTestApp.Views;
 
@@ -7,5 +10,31 @@ public partial class TerminalView : UserControl
     public TerminalView()
     {
         InitializeComponent();
+    }
+
+    private void OnLoaded(object sender, RoutedEventArgs e)
+    {
+        Terminal.LineEntered += OnLineEntered;
+        if (DataContext is TerminalViewModel vm)
+        {
+            vm.StartProcess();
+        }
+    }
+
+    private void OnUnloaded(object sender, RoutedEventArgs e)
+    {
+        Terminal.LineEntered -= OnLineEntered;
+        if (DataContext is TerminalViewModel vm)
+        {
+            vm.StopProcess();
+        }
+    }
+
+    private void OnLineEntered(object? sender, EventArgs e)
+    {
+        if (DataContext is TerminalViewModel vm && Terminal.Line != null)
+        {
+            vm.ExecuteLine(Terminal.Line);
+        }
     }
 }
