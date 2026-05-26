716781ea225d7819a5d67c224c3b7c144e1592cf
ALEU1C014278 <stefan.kneidl@alpsalpine.com>
2026-05-26 22:13:05 +0200

    fix styling

--- source/AvalonDockCodeApp/Views/TerminalView.xaml.cs
+++ source/AvalonDockCodeApp/Views/TerminalView.xaml.cs
@@ -8,37 +8,37 @@ namespace ToggleTestApp.Views;
 
 public partial class TerminalView : UserControl
 {
-    public TerminalView()
-    {
-        InitializeComponent();
-        DataContextChanged += OnDataContextChanged;
-    }
+	public TerminalView()
+	{
+		InitializeComponent();
+		DataContextChanged += OnDataContextChanged;
+	}
 
-    private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
-    {
-        if (e.OldValue is TerminalViewModel oldVm)
-            oldVm.PropertyChanged -= OnTerminalPropertyChanged;
+	private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+	{
+		if (e.OldValue is TerminalViewModel oldVm)
+			oldVm.PropertyChanged -= OnTerminalPropertyChanged;
 
-        if (e.NewValue is TerminalViewModel newVm)
-            newVm.PropertyChanged += OnTerminalPropertyChanged;
-    }
+		if (e.NewValue is TerminalViewModel newVm)
+			newVm.PropertyChanged += OnTerminalPropertyChanged;
+	}
 
-    private void OnTerminalPropertyChanged(object? sender, PropertyChangedEventArgs e)
-    {
-        if (e.PropertyName == nameof(TerminalViewModel.Output))
-        {
-            OutputScroller.ScrollToEnd();
-        }
-    }
+	private void OnTerminalPropertyChanged(object? sender, PropertyChangedEventArgs e)
+	{
+		if (e.PropertyName == nameof(TerminalViewModel.Output))
+		{
+			OutputScroller.ScrollToEnd();
+		}
+	}
 
-    protected override void OnPreviewKeyDown(KeyEventArgs e)
-    {
-        if (e.Key == Key.Enter && DataContext is TerminalViewModel vm)
-        {
-            vm.SendCommandCommand.Execute(null);
-            e.Handled = true;
-        }
+	protected override void OnPreviewKeyDown(KeyEventArgs e)
+	{
+		if (e.Key == Key.Enter && DataContext is TerminalViewModel vm)
+		{
+			vm.SendCommandCommand.Execute(null);
+			e.Handled = true;
+		}
 
-        base.OnPreviewKeyDown(e);
-    }
+		base.OnPreviewKeyDown(e);
+	}
 }
