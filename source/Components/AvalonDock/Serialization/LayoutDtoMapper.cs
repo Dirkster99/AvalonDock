5c9ec02ea923b316fb1ced027a4863e05180e2d0
sharpSteff <sharp.kneidl@gmail.com>
2026-06-06 20:45:16 +0200

    cleanup

--- source/Components/AvalonDock/Serialization/LayoutDtoMapper.cs
+++ source/Components/AvalonDock/Serialization/LayoutDtoMapper.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using AvalonDock.Core.Serialization;
